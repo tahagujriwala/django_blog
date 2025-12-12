6599cba9ca72c4af5f59b3e82a11f1876be44037
tahagujri <tahagujri7@gmail.com>
2025-12-12 12:39:56 +0530

    post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def post_list(request):
+#     return render(request, 'blog/post_list.html', {})
+from .models import Post
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
