3811274a837729d31235304c0cc72454f41f5e0f
tahagujri <tahagujri7@gmail.com>
2025-12-12 15:14:45 +0530

    applied crud operation on post

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 # def post_list(request):
 #     return render(request, 'blog/post_list.html', {})
 from .models import Post
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
@@ -13,3 +16,49 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)  # Do not save to DB yet
+            post.author = request.user      # Set the author
+            post.save()                     # Now save
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    # Only the author can edit
+    if post.author != request.user:
+        return redirect('post_detail', pk=post.pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_form.html', {'form': form})
+
+
+@login_required
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    if post.author != request.user:
+        return redirect('post_detail', pk=post.pk)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
